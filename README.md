1c0aa9a7dc93884a9259cf1745a79de9a19ac8fa
20pivot <20pivot@gmail.com>
2022-02-25 13:32:52 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-ot3cvo
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-ot3cvo)
---
+++ main.js
@@ -0,0 +1,69 @@
+var state = 'press-key';
+var patron = [];
+var level = 0;
+var indexPlayerPatron = 0;
+
+var title = document.getElementById('title');
+var red = document.getElementById('red');
+var green = document.getElementById('green');
+var blue = document.getElementById('blue');
+var yellow = document.getElementById('yellow');
+
+var button = [red, green, blue, yellow];
+
+document.addEventListener('keypress', startGame);
+red.addEventListener('click', buttonPress);
+green.addEventListener('click', buttonPress);
+blue.addEventListener('click', buttonPress);
+yellow.addEventListener('click', buttonPress);
+
+function startGame() {
+  if (state === 'press-key' || state === 'gameover') {
+    newLevel();
+    patron = [];
+    level = 0;
+    indexPlayerPatron = 0;
+  }
+}
+
+function newLevel() {
+  state = 'waitting-patron';
+  setTimeout(() => {
+    level = level + 1;
+    title.innerText = 'Nivel ' + level;
+    var nextColor = Math.floor(Math.random() * 4);
+    var nextButton = button[nextColor];
+
+    lightButton(nextButton);
+    patron.push(nextButton);
+
+    indexPlayerPatron = 0;
+
+    state = 'waitting-player';
+  }, 500);
+}
+
+function lightButton(button) {
+  button.classList.toggle('active');
+  setTimeout(() => {
+    button.classList.toggle('active');
+  }, 200);
+}
+
+function buttonPress(event) {
+  if (state === 'waitting-player') {
+    var button = event.target;
+    if (button === patron[indexPlayerPatron]) {
+      indexPlayerPatron = indexPlayerPatron + 1;
+      lightButton(button);
+      if(indexPlayerPatron === patron.length) {
+        newLevel();
+      }
+    } else {
+      state = 'gameover';
+      title.innerText = 'gameover';
+    }
+  }
+}
+
+console.clear()
